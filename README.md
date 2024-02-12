40f6ce89946f5413045b2a3166352897816abd13
Leonardo Alvarenga <102129665+ynfleozin@users.noreply.github.com>
2024-02-12 20:54:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ Acesso ao meu portfólio pessoal. Aqui você encontrará uma coleção dos meus projetos mais recentes e relevantes, abrangendo uma variedade de áreas.  
---
+++ assets/js/menu.js
@@ -0,0 +1,15 @@
+let btnMenu = document.getElementById('abrir')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
