8cc782dd4f41502d61a186836a985762bfbda5ed
ynfleozin <leonardoalvarengam@gmail.com>
2024-12-01 22:23:25 -0300

    remove projects box

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -10,11 +10,11 @@ const Info = () => {
                 <span className="about__subtitle">1 Year</span>
             </div>
 
-            <div className="about__box">
+            {/* <div className="about__box">
             <i class="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Completed</h3>
                 <span className="about__subtitle">20 + Projects</span>
-            </div>
+            </div> */}
 
             <div className="about__box">
             <i class="bx bx-support about__icon"></i>
