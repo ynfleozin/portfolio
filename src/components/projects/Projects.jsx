1b4fa119e73c8ef0c5304c6530704244da94e971
ynfleozin <leonardoalvarengam@gmail.com>
2024-12-02 19:12:41 -0300

    add new project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,6 +5,7 @@ import granRomanoImg from '../../assets/img/projects/granromano.webp';
 import shTecnoImg from '../../assets/img/projects/shtecno.webp';
 import fitnessImg from '../../assets/img/projects/fitnesstracker.webp'
 import urlImg from '../../assets/img/projects/URL-shortener.webp'
+import homyzImg from '../../assets/img/projects/homyz.webp'
 
 const Projects = () => {
   const projectList = [
@@ -22,16 +23,22 @@ const Projects = () => {
       description: 'This project is a corporate website developed for the company SH Tecnologia, specialized in services in the technological area. The website was created with the aim of presenting detailed information about the company, partners and their services, as well as facilitating contact with customers.',
     },
     {
-      link: '#',
+      link: 'https://github.com/ynfleozin/fitness-project',
       image: fitnessImg, 
       title: 'Fitness Tracker',
       description: 'This is a fitness project developed to help users monitor their workouts, activities, and personal goals. The application is built using Spring Boot and Hibernate for the back-end with MySQL as the database, and Angular for the front-end, providing an intuitive interface to manage and visualize fitness progress.',
     },
     {
-      link: '#',
+      link: 'https://github.com/ynfleozin/URL-shortener',
       image: urlImg, 
       title: 'URL Shortener',
       description: 'This project is a URL shortener system using AWS Lambda, S3, Amazon API Gateway, and Java.',
+    },
+    {
+      link: 'https://site-homyz.vercel.app/',
+      image: homyzImg, 
+      title: 'Homyz',
+      description: 'Real estate website project developed with React, with smooth navigation and responsive design.',
     },
   ];
 
